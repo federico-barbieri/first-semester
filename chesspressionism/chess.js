10c754dbb49fbcaea0f8cda1704dc0dd3fe6e0d5
Federico Barbieri <federicobarbieri54@gmail.com>
2022-06-12 09:39:36 +0200

    Add files via upload

--- chesspressionism/chess.js
+++ chesspressionism/chess.js
@@ -8,36 +8,58 @@ let topicSpan = document.querySelector('.topics-span');
 
 
 function createMath(){
+    topicSpan.style.fontFamily = "ohm-bold, sans serif";
     topicSpan.innerHTML = "MATHS";
+    
 }
 
 function createCancel(){
+    topicSpan.style.fontFamily = "bungee, sans serif";
     topicSpan.innerHTML = "CANCEL CULTURE";
+    
 }
 
 function createArt(){
+    topicSpan.style.fontFamily = "all-round-gothic, sans serif";
     topicSpan.innerHTML = "ART";
+    
+    
 }
 
 function createPhilo(){
+    
+    topicSpan.style.fontFamily = "courage, sans serif";
     topicSpan.innerHTML = "PHILOSOPHY";
 }
 
 function createHistory(){
+    
+    topicSpan.style.fontFamily = "poiret-one, sans serif";
     topicSpan.innerHTML = "HISTORY";
 }
 
 function createTech(){
+    
+    topicSpan.style.fontFamily = "nasalization, sans serif";
     topicSpan.innerHTML = "TECHNOLOGY";
 }
 
+function createScience(){
+    
+    topicSpan.style.fontFamily = "ff-nuvo-mono-web-pro, sans serif";
+    topicSpan.innerHTML = "SCIENCE";
+}
+
+
 function everything(){
-    setTimeout(createMath, 1000);
-    setTimeout(createCancel, 2000);
-    setTimeout(createArt, 3000);
-    setTimeout(createPhilo, 4000);
-    setTimeout(createHistory, 5000);
-    setTimeout(everything, 6000);
+    setTimeout(createMath, 100);
+    setTimeout(createCancel, 1000);
+    setTimeout(createArt, 2000);
+    setTimeout(createPhilo, 3000);
+    setTimeout(createHistory, 4000);
+    setTimeout(createTech, 5000);
+    setTimeout(createScience, 6000);
+    setTimeout(everything, 7000);
 }
 
 setTimeout(everything, 1000);
